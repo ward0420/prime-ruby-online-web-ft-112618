367e69b891931cc2f69e3f98550acc656100b79d
Don Ward <10is0420@gmail.com>
2018-12-08 01:16:59 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,18 @@
-# Add  code here!
+def prime?(numbers)
+  
+  return false numbers < 3
+  
+  return numbers.collect { |i| 
+  
+    if i % (3..11) == 0
+      
+    false
+    
+    else 
+    
+    true
+
+  end
+  }
+
+end
