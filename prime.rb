6eb35cd259a3f40d553f4671dab613e171755ef3
Don Ward <10is0420@gmail.com>
2018-12-08 13:35:55 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,21 @@
-# Add  code here!
+def prime?(numbers)
+  
+  if numbers < 2
+    
+    false
+  
+  elsif (12...42).any? {|i| numbers % i == 0}
+      
+    false
+    
+  elsif numbers == 4
+  
+    false
+    
+  else 
+    
+    true
+
+  end
+
+end
